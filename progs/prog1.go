7c4cc10554fd7aa8681b7ec66be1f70c44e2e06e
Brett Binnersley <Brettbinnersley14@gmail.com>
2016-03-12 12:45:26 -0800

    added progs

---
+++ progs/prog1.go
@@ -0,0 +1,20 @@
+package main
+
+var z, za float64
+var zb, zc = 3,7
+const n int32 = 99
+type fptr *float32
+type a_slice []int64
+var x, y int = 3, 4
+const a,b = 7,8
+const (
+	win            = 100 // The winning score in a game of Pig
+	gamesPerSeries = 10  // The number of games per series to simulate
+)
+
+type BookInfo struct { subject string; book_id int }
+type Books struct { title string; author string; info BookInfo }
+func main(k int) float32{
+    var Book1 Books
+    Book1.title = "Go Programming"
+}
